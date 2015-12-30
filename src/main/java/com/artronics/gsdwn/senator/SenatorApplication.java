30e48d501f3656a7090b1cbdea5647c498502668
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2015-12-30 16:50:42 +0100

    change all versions to 0.0.2-SNAPSHOT and remove senator spring app

--- src/main/java/com/artronics/gsdwn/senator/SenatorApplication.java
+++ src/main/java/com/artronics/gsdwn/senator/SenatorApplication.java
@@ -1,12 +1,7 @@
 package com.artronics.gsdwn.senator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class SenatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SenatorApplication.class, args);
 	}
 }
